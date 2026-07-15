93d045bdc94223ff27cfb5865a04d9d67e8ccccf
DanTunsoiuSoftwire <Dan.Tunsoiu@softwire.com>
2026-07-15 12:18:37 +0300

    Made pawn move 2 spaces for its first move

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -4,19 +4,32 @@ import Board from '../board';
 import Square from "../square";
 
 export default class Pawn extends Piece {
+    private direction: number;
+
     public constructor(player: Player) {
         super(player);
+        if (player == Player.WHITE) {
+            this.direction = 1;
+        } else {
+            this.direction = -1;
+        }
+    }
+
+    public addFirstMove(moves: Array<Square>) : Array<Square> {
+        if (!this.moved) {
+            let previousAvailableMove: Square = moves[0];
+            moves.push(new Square(previousAvailableMove.row + this.direction, previousAvailableMove.col));
+        }
+
+        return moves;
     }
 
     public getAvailableMoves(board: Board) : Array<Square> {
         let moves : Array<Square> = [];
         let currentSquare : Square = board.findPiece(this);
 
-        if (this.player == Player.WHITE) {
-            moves.push(new Square(currentSquare.row + 1, currentSquare.col));
-        } else {
-            moves.push(new Square(currentSquare.row - 1, currentSquare.col));
-        }
+        moves.push(new Square(currentSquare.row + this.direction, currentSquare.col));
+        moves = this.addFirstMove(moves);
 
         return moves;
     }
