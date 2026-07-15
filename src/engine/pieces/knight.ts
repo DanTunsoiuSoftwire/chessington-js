a56e9406b5009700ace5bd96727a17f294c77075
DanTunsoiuSoftwire <Dan.Tunsoiu@softwire.com>
2026-07-15 14:03:42 +0300

    Added moves for horse

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,13 +1,30 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    private addLshapePossibleMoves(moves: Array<Square>, currentSquare : Square) {
+        for (let currentCol = 2; currentCol > -3; currentCol--) {
+            for (let currentRow = 2; currentRow > -3; currentRow--) {
+                if (currentCol === currentRow || currentCol * currentRow === 0 || currentCol === -currentRow) {
+                    continue;
+                }
+                moves.push(new Square(currentSquare.row + currentRow, currentSquare.col + currentCol));
+            }
+        }
+    }
+
+    public getAvailableMoves(board: Board) : Array<Square> {
+        let moves : Array<Square> = [];
+        let currentSquare : Square = board.findPiece(this);
+
+        this.addLshapePossibleMoves(moves, currentSquare);
+
+        return moves;
     }
 }
