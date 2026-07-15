8eebea80e5e067da98e6b1fabad5e4d9d630895d
DanTunsoiuSoftwire <Dan.Tunsoiu@softwire.com>
2026-07-15 11:39:36 +0300

    Finnished the first pawn tests!!

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,13 +1,23 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board) : Array<Square> {
+        let moves : Array<Square> = [];
+        let currentSquare : Square = board.findPiece(this);
+
+        if (this.player == Player.WHITE) {
+            moves.push(new Square(currentSquare.row + 1, currentSquare.col));
+        } else {
+            moves.push(new Square(currentSquare.row - 1, currentSquare.col));
+        }
+
+        return moves;
     }
 }
