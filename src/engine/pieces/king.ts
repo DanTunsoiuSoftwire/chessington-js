e59cd5a018407bfd6e3f0bba86ee8c78080ad2b1
DanTunsoiuSoftwire <Dan.Tunsoiu@softwire.com>
2026-07-15 16:21:46 +0300

    Added Moves for King

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,13 +1,30 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    private addMovesAroundPiece(moves: Array<Square>, currentSquare : Square) {
+        for (let currentRow = -1; currentRow < 2; currentRow++) {
+            for (let currentCol = -1; currentCol < 2; currentCol++) {
+                if (currentRow + currentCol === 0 && currentCol === currentRow) {
+                    continue;
+                }
+                moves.push(new Square(currentSquare.row + currentRow, currentSquare.col + currentCol));
+            }
+        }
+    }
+
+    public getAvailableMoves(board: Board) : Array<Square> {
+        let moves : Array<Square> = [];
+        let currentSquare : Square = board.findPiece(this);
+
+        this.addMovesAroundPiece(moves, currentSquare);
+
+        return moves;
     }
 }
